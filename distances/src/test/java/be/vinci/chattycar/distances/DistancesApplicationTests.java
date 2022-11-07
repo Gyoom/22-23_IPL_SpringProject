c7cd9657742fedd498c639a57ae286df7848af12
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2022-11-07 12:44:47 +0100

    creation of .yaml for users and distances

---
+++ distances/src/test/java/be/vinci/chattycar/distances/DistancesApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vinci.chattycar.distances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
