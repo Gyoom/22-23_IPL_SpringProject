077e93573d8241c96d68db54803f242258c66100
François Bardijn <francois.bardijn@hotmail.com>
2022-11-07 11:00:39 +0100

    [ADD] passengers module

---
+++ passengers/src/main/java/be/vinci/ipl/chattycar/passengers/PassengersApplication.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.chattycar.passengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PassengersApplication.class, args);
+  }
+
+}
---
+++ passengers/src/test/java/be/vinci/ipl/chattycar/passengers/PassengersApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.chattycar.passengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengersApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
