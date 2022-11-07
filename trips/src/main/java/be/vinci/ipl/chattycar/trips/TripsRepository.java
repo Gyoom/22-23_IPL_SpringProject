9cb1182b6fe4ab519e188ad0b5764f638d73b360
Guill <guillaume.piette@student.vinci.be>
2022-11-07 15:44:39 +0100

    delivrable2 : trip+ auth

---
+++ trips/src/main/java/be/vinci/ipl/chattycar/trips/TripsRepository.java
@@ -0,0 +1,16 @@
+package be.vinci.ipl.chattycar.trips;
+
+import be.vinci.ipl.chattycar.trips.models.Position;
+import be.vinci.ipl.chattycar.trips.models.Trip;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripsRepository extends CrudRepository<Trip, Integer> {
+
+  boolean existsByOriginAndDestinationAndDepartureAndDriver_id(Position origin, Position destination, String departure, int driverId);
+
+  Iterable<Trip> findByDriver_id(int driverId);
+
+}
