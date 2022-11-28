fad2458129658845f4fc2fdb86651f3984609613
Eliott Rasumny <eliott.rasumny@student.vinci.be>
2022-11-28 11:05:29 +0100

    edit distance service

--- distances/src/main/java/be/vinci/chattycar/distances/DistancesController.java
+++ distances/src/main/java/be/vinci/chattycar/distances/DistancesController.java
@@ -1,15 +1,17 @@
 package be.vinci.chattycar.distances;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.ws.rs.QueryParam;
 
 @RestController
 public class DistancesController {
     @GetMapping("/distances")
-    public double distanceBetween(@QueryParam("lat1") double lat1, @QueryParam("lon1") double lon1, @QueryParam("lat2") double lat2, @QueryParam("lon2") double lon2) {
-        //TODO(Eliott) est ce qu'il faut checker si les QueryParams sont là ou bien est ce qu'il le fait tout seul vu que j'ai mis dans le .yaml "required: true"
+    public double distanceBetween(@QueryParam("lat1") Double lat1, @QueryParam("lon1") Double lon1, @QueryParam("lat2") Double lat2, @QueryParam("lon2") Double lon2) {
+        if (lat1 == null || lat2 == null || lon1 == null || lon2 == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         return Math.sqrt(Math.pow(lat1-lat2, 2) + Math.pow(lon1-lon2, 2));
     }
 
